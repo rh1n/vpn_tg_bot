e21c6ebe7f75eef7905eb7c37e0c5eb6ca496d7b
rh1n <97663230+rh1n@users.noreply.github.com>
2026-06-04 20:48:53 +0300

    Create logger.py

---
+++ app/utils/logger.py
@@ -0,0 +1,33 @@
+# app/utils/logger.py
+import structlog
+import logging
+import sys
+
+def configure_logger():
+    """Configure structlog for JSON logging in production"""
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.processors.JSONRenderer(),
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    # Configure root logger
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.INFO,
+    )
+
+logger = structlog.get_logger(__name__)
